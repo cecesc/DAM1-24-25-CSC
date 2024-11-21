3fb6ed17c6b00d5e187c96b194a36b3f497be9b9
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2024-11-21 11:30:37 +0100

    g

--- src/ud2/cscexamen/ChiclesRegalo.java
+++ src/ud2/cscexamen/ChiclesRegalo.java
@@ -1,6 +1,9 @@
 //Autor: Celia Sanmartín Chapela
 package ud2.cscexamen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
 public class ChiclesRegalo {
     public static int totalChicles(int chiclesComprados, int envoltoriosReunir, int totalChiclesGratis) {
 
@@ -19,5 +22,16 @@ public class ChiclesRegalo {
 
     public static void main(String[] args) {
         System.out.println(totalChicles(0, 0, 0));
+    }
+
+    @Test
+    public void ChiclesRegaloTest() {
+        assertEquals(31, totalChicles(25, 5, 1));
+        assertEquals(6, totalChicles(5, 5, 1));
+        assertEquals(111, totalChicles(100, 10, 1));
+        assertEquals(100, totalChicles(100, 0, 0));
+        assertEquals(-1, totalChicles(20, 2, 5));
+        assertEquals(-1, totalChicles(10, -1, 0));
+
     }
 }
--- src/ud2/cscexamen/DuracionBombillas.java
+++ src/ud2/cscexamen/DuracionBombillas.java
@@ -1,6 +1,9 @@
 //autor: Celia Sanmartín Chapela
 package ud2.cscexamen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
 public class DuracionBombillas {
 
     public static String causaFinBombilla(int maxHoras, int maxEncendidos, int maxHorasEncendida) {
@@ -23,6 +26,22 @@ public class DuracionBombillas {
         int maxHoras = 0, maxEncendidos = 0, maxHorasEncendida = 0;
         System.out.println(causaFinBombilla(maxHoras, maxEncendidos, maxHorasEncendida));
 
+    }
+
+    @Test
+    public void causaFinBombillaTest() {
+        assertEquals("ERROR", causaFinBombilla(100, 100, 100));
+        assertEquals("ERROR", causaFinBombilla(1000, -10, 10));
+        assertEquals("ERROR", causaFinBombilla(0, 10, 10));
+        assertEquals("ERROR", causaFinBombilla(1000, 10, 0));
+        assertEquals("ENCENDIDOS", causaFinBombilla(500, 400, 1));
+        assertEquals("HORAS", causaFinBombilla(1000, 10000, 1));
+        assertEquals("HORAS", causaFinBombilla(1000, 200, 10));
+        assertEquals("HORAS", causaFinBombilla(2000, 700, 3));
+        assertEquals("ENCENDIDOS", causaFinBombilla(2000, 600, 3));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 1));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 9));
+        assertEquals("ENCENDIDOS + HORAS", causaFinBombilla(1000, 100, 10));
     }
 
 }
