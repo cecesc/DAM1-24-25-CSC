c7ff82cfd07bee31ab4768df379096d1ac76c5a3
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2024-11-22 14:21:20 +0100

    empezando unidad tres, sin completar

---
+++ src/ud3/apuntes/Persona.java
@@ -0,0 +1,46 @@
+package ud3.apuntes;
+
+public class Persona {
+    // Datos
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+    private double estatura = 1.60;
+    private final String dni = null;
+
+    public static int contadorPersonas;
+
+    // Métodos
+    public void inicializar(String n, String a1, String a2, int e) {
+        nombre = n;
+        apellido1 = a1;
+        apellido2 = a2;
+        edad = e;
+    }
+
+    public String nombreCompleto() {
+        return nombre + " " + apellido1 + " " + apellido2;
+    }
+
+    public boolean esMayorEdad() {
+        if (edad >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void cumplirAnhos() {
+        edad++;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public static void incrementarContador() {
+        contadorPersonas++;
+    }
+
+}
