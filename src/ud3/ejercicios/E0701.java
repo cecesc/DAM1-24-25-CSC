50be9500a09eed65c98b068543a0b674a08b873d
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2024-11-26 13:58:39 +0100

    intento de hacer un ejercicio de la unidad 3

---
+++ src/ud3/ejercicios/E0701.java
@@ -0,0 +1,20 @@
+/* Diseñar la clase cuentacorriente, que almacena los datos: DNI y nombre del titular, asi como el saldo. Las operaciones típicas con una cuenta corriente son:
+
+● Crear una cuenta: se necesita el DNI y nombre del titular. El saldo inicial será 0.
+
+● Sacar dinero: el método debe indicar si ha sido posible llevar a cabo la operación, si existe saldo suficiente.
+
+● Ingresar dinero: se incrementa el saldo.
+
+● Mostrar información: muestra la información disponible de la cuenta corriente. */
+
+package ud3.ejercicios;
+
+public class E0701 {
+    private float dni;
+    private String nombreTitular;
+    private double saldo;
+    private int edad;
+        Mascota perro = new Mascota(float dni, String nombreTitular, double saldo);
+    }
+
