b78757ddeee559732cd7976e0eaf3c4be014c7d2
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2024-11-28 10:20:53 +0100

    Títulos

--- src/ud3/ejerciciosclases/EP0711.java
+++ src/ud3/ejerciciosclases/EP0711.java
@@ -1,3 +1,18 @@
+/*
+Ejercicios para hacer en java
+EP0711. Escribe la clase MarcaPagina, que ayuda a llevar el control de la lectura de un
+libro. Deberá disponer de métodos para incrementar la página leída, para obtener
+información de la última página que se ha leído y para comenzar desde el principio una
+nueva lectura del mismo libro.
+
+EP0712. Implementa una clase que permita resolver ecuaciones de segundo grado. Los
+coeficientes pueden indicarse en el constructor y modificarse a posteriori. Es fundamental
+que la clase disponga de un método que devuelva las distintas soluciones y de un método
+que nos informe si el discriminante es positivo.
+
+EP0714. Crea una clase que sea capaz de mostrar el importe de un cambio, por ejemplo, al
+realizar una compra, con el menor número de monedas y billetes posibles. */
+
 package ud3.ejerciciosclases;
 
 public class EP0711 {
