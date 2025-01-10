4164215e95c457c666b60f6034a0dfcd6043a074
cecesc <celia.sanmartin.chapela@gmail.com>
2025-01-10 11:16:39 +0100

    Actualizar ud4

---
+++ src/ud4/E15.java
@@ -0,0 +1,18 @@
+public class EjArrays15 {
+    public static int[][] sumaMatrices(int[][] m1, int[][] m2) {
+        if (m1 == null || m2 == null || m1.length == 0 || m2.length == 0 || m1.length != m2.length || m1[0].length != m2[0].length) {
+            return null; // Manejo de matrices nulas o de tamaños diferentes
+        }
+
+        int filas = m1.length;
+        int columnas = m1[0].length;
+        int[][] suma = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return suma;
+    }
+}
