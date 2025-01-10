4baee7e6d639693c4e7fe9e65ed0cabd3b2e8ab9
cecesc <celia.sanmartin.chapela@gmail.com>
2025-01-10 11:17:05 +0100

    Actualizar ud4

---
+++ src/ud4/E16.java
@@ -0,0 +1,20 @@
+public class EjArrays16 {
+    public static int[][] multiplicar(int[][] t1, int[][] t2) {
+        if (t1 == null || t2 == null || t1.length == 0 || t2.length == 0 || t1[0].length != t2.length) {
+            return null; // Manejo de matrices nulas o incompatibles
+        }
+
+        int filasT1 = t1.length;
+        int columnasT2 = t2[0].length;
+        int[][] producto = new int[filasT1][columnasT2];
+
+        for (int i = 0; i < filasT1; i++) {
+            for (int j = 0; j < columnasT2; j++) {
+                for (int k = 0; k < t1[0].length; k++) {
+                    producto[i][j] += t1[i][k] * t2[k][j];
+                }
+            }
+        }
+        return producto;
+    }
+}
