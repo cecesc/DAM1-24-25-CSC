eccabe01d76b3b563126c0b3610016bb88dc0ebb
cecesc <celia.sanmartin.chapela@gmail.com>
2025-01-10 11:11:21 +0100

    Actualizar ud4

---
+++ src/ud4/E9.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class EjArrays09 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numerosAleatorios = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numerosAleatorios[i] = random.nextInt(100); // Números aleatorios entre 0 y 99
+        }
+
+        System.out.println("Números aleatorios generados:");
+        for (int num : numerosAleatorios) {
+            System.out.print(num + " ");
+        }
+    }
+}
