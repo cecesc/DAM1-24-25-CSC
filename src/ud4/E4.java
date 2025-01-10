d424f71aa3c0a1ec032576de6437c48dc1a36a9f
cecesc <celia.sanmartin.chapela@gmail.com>
2025-01-10 11:08:30 +0100

    Actualizar ud4

---
+++ src/ud4/E4.java
@@ -0,0 +1,14 @@
+public class EjArrays04 {
+    public static void main(String[] args) {
+        int[] pares = new int[20];
+
+        for (int i = 0; i < 20; i++) {
+            pares[i] = i * 2;
+        }
+
+        System.out.println("Los primeros 20 números pares son:");
+        for (int par : pares) {
+            System.out.print(par + " ");
+        }
+    }
+}
