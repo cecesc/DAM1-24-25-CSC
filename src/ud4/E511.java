eba90ae476858a7b9c07532bb7391e8aceac6b15
cecesc <celia.sanmartin.chapela@gmail.com>
2025-01-10 11:21:04 +0100

    Actualizar ud4

---
+++ src/ud4/E511.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class EP0511 {
+    public static int[] buscarTodos(int[] t, int clave) {
+        ArrayList<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < t.length; i++) {
+            if (t[i] == clave) {
+                indices.add(i);
+            }
+        }
+        return indices.stream().mapToInt(i -> i).toArray(); // Convertir a array
+    }
+
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 2, 4, 2};
+        int clave = 2;
+        int[] indices = buscarTodos(t, clave);
+        System.out.println("Indices de la clave " + clave + ": " + java.util.Arrays.toString(indices));
+    }
+}
