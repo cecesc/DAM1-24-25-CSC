52c9d7c5e3594f42b419aac9eca4c6994a5eef97
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-02-02 13:40:04 +0100

    Añadidos y corregidos

--- src/ud4/apuntes/stringsapuntes/E0615.java
+++ src/ud4/apuntes/stringsapuntes/E0615.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 
 public class E0615 {
     public static void main(String[] args) {
+        // Leer Palabra Original
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce una palabra: ");
+        System.out.print("Jugador 1: Introduzca una palabra: ");
         String palabra;
         do {
             palabra = sc.nextLine();
         } while (palabra.isEmpty());
 
+        // Generar Anagrama
         char[] letras = palabra.toCharArray();
         for (int i = 0; i < letras.length; i++) {
             Random rnd = new Random(0);
@@ -21,35 +23,41 @@ public class E0615 {
             letras[nuevaPosicion] = letras[i];
             letras[i] = letra;
         }
-
         String anagrama = String.valueOf(letras);
         System.out.println("Anagrama: " + anagrama);
 
+        // System.out.println(E0612.sonAnagrama(palabra, anagrama));
+
+        // El jugador 2 debe adivinar la palabra
         final int MAX_INTENTOS = 3;
         int intentos = 0;
-
-        String palabraj2;
+        String palabraJ2;
         do {
-            System.out.println("Jugador 2:  Adivina la palabra original: ");
-            palabraj2 = sc.nextLine();
-            intentos++ ;
+            System.out.println("Jugador 2: Adivina la palabra original. Te quedan " + (MAX_INTENTOS - intentos) + " intentos.");
+            palabraJ2 = sc.nextLine();
+            intentos++;
 
-        } while (!palabraj2.equalsIgnoreCase(palabra)&& intentos < MAX_INTENTOS);
+            int coincidencias = contarCaracteresIguales(palabra, palabraJ2);
+            System.out.println("Caracteres que coinciden en el mismo lugar: " + coincidencias);
 
-        
-        if (!palabraj2.equalsIgnoreCase(palabra)) {
-            System.out.println("geniall");
+        } while (!palabraJ2.equalsIgnoreCase(palabra) && intentos < MAX_INTENTOS);
+
+        if (palabraJ2.equalsIgnoreCase(palabra)) {
+            System.out.println("Enhorabuena!! Has acertado en " + intentos + " intentos.");
         } else {
-            System.out.println("No hays acertado");
+            System.out.println("No has acertado!! y has consumido tus " + intentos + " intentos.");
         }
-        
-        
-        
-        sc.close();
-
     }
 
-    private static int contarCaracteresIguales(String palabra, String palabraJ2){
-
+    private static int contarCaracteresIguales(String palabra, String palabraJ2) {
+        int coincidencias = 0;
+        int repeticiones = Math.min(palabra.length(), palabraJ2.length());
+        for (int i = 0; i < repeticiones; i++) {
+            if (palabra.charAt(i) == palabraJ2.charAt(i)) {
+                coincidencias++;
+            }
+        }
+        return coincidencias;
     }
+
 }
