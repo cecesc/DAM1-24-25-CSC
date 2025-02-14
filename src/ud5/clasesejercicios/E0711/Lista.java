7f0776235596ae84b1612d0a37b9df6acd09445f
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-02-14 14:23:44 +0100

    empiezo

--- src/ud5/clasesejercicios/E0711/Lista.java
+++ src/ud5/clasesejercicios/E0711/Lista.java
@@ -1,29 +1,17 @@
 package ud5.clasesejercicios.E0711;
 
 public class Lista {
-    int numeros;
-    int tabla;
-    
+    private Integer[] elementos;    
 
-    public Lista(int numeros) {
-        this.numeros = numeros;
-
-        for (int i = 0; i < this.numeros; i++) {
-            int suma =+ numeros;
-             
-        }
-
-        for (int i = 0; i > this.numeros; i++) {
-
-        
-        }
+    public Lista() {
+        elementos = new Integer[0];
     }
 
-
-    public static void main(String[] args) {
-        System.out.println();
-
+    public int length(){
+        return elementos.length;
     }
+
+   
 
    
 
