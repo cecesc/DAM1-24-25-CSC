7974732f78c33de9ce4365c598ef99a6a1d5fbca
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-02-26 11:25:36 +0100

    No se ni lo que estoy haciendo

---
+++ src/contornos/ud3/PruebasTest.java
@@ -0,0 +1,26 @@
+package contornos.ud3;
+
+import org.junit.jupiter.api.Test;
+
+public class PruebasTest {
+
+
+    @Test
+    void testEsNumeroPerfecto() {
+    boolean resultadoEsperado= true;
+    Pruebas prueba = new Pruebas();
+    boolean resultado= Pruebas.esNumeroPerfecto(5);
+           
+    }
+
+
+    
+    @Test
+    void contarParesImpares() {
+    boolean resultadoEsperado= true;
+    Pruebas prueba = new Pruebas();
+    boolean resultado= Pruebas.contarParesImpares();
+           
+    }
+
+}
--- src/ud5/apuntesherencia/ejemploabstract2/Poligono.java
+++ src/ud5/apuntesherencia/ejemploabstract2/Poligono.java
@@ -1,38 +1,42 @@
 package ud5.apuntesherencia.ejemploabstract2;
 
+//Clase abstracta Poligono
 public abstract class Poligono {
-private int numLados;
 
+    private int numLados;
 
-public Poligono(){
-
-}
-public Poligono(int numLados){
-this.numLados= numLados;
-}
-
-public int getNumLados(){
-    return numLados;
-}
-
-public void setNumLados(int numLados){
-    this.numLados=numLados;
-}
-
-//Declaracion del método  abstracto area()
-public abstract double area();
-public static void main(String[] args) {
-    Triangulo t = new Triangulo();
-    System.out.printf("Area del triangulo %.2f %n" , t.area());
-    Rectangulo r = new Rectangulo();
-    System.out.printf("Area del rectangulo %.2f %n" , r.area());
-
-    Poligono[] poligonos={t,r};
-    for (Poligono poligono : poligonos) {
-        System.out.println(poligono.area());
-        
+    public Poligono() {
     }
-    Triangulo t2=(Triangulo) poligonos[1];
 
-}
+    public Poligono(int numLados) {
+        this.numLados = numLados;
+    }
+
+    public int getNumLados() {
+        return numLados;
+    }
+
+    public void setNumLados(int numLados) {
+        this.numLados = numLados;
+    }
+    //Declaración del método abstracto area()
+    public abstract double area();
+
+
+    public static void main(String[] args) {
+        Triangulo t = new Triangulo(3.25,4.55,2.71);
+        System.out.printf("Área del triángulo: %.2f %n" , t.area());
+        Rectangulo r = new Rectangulo(5.70,2.29);
+        System.out.printf("Área del rectángulo: %.2f %n" , r.area());
+
+        Poligono[] poligonos = {t, r};
+        for (Poligono poligono : poligonos) {
+            System.out.println(poligono.area());
+        }
+
+        Triangulo t2 = (Triangulo) poligonos[1];
+
+    }
+
+
 }
