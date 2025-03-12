9d3910ffda2734331bb2ee8bd85b1c0969102db5
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-03-12 14:12:45 +0100

    corregir todo esto

---
+++ src/ud5/apuntesinterfaz/ComparaSociosFecha.java
@@ -0,0 +1,13 @@
+package ud5.apuntesinterfaz;
+
+import java.util.Comparator;
+
+public class ComparaSociosFecha implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        Socio s1 = (Socio) o1;
+        Socio s2 = (Socio) o2;
+        return s1.fechaAlta.compareTo(s2.fechaAlta);
+    }
+
+}
