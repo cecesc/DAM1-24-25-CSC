1046b1801a9c64c6926199b98ff2094934b792d5
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-05-07 14:21:24 +0200

    Nueva unidad 7 y cambio de jdk

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    static int contador = 0;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! My name is CSC");
+        System.out.println("Hello, JavaFX!");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btn = new Button("Púlsame");
+        btn.setOnAction(e -> {
+            System.out.println("Botón pulsado!");
+            contador++;
+            if (contador > 1)
+                btn.setText("Boton pulsado " + contador + " veces");
+            else
+                btn.setText("Boton pulsado");
+        });
+        Scene scene = new Scene(btn, 300, 200);
+        stage.setTitle("Hola, JavaFX");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
     }
 }
---
+++ src/ud7/prueba.java
@@ -0,0 +1,15 @@
+package ud7;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class prueba {
+        public static void main(String[] args) {
+        List<Integer> numbers = new LinkedList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+    System.out.println(numbers.get(1));
+        }
+}
