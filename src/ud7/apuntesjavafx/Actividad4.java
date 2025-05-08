4f3ba052bbb5d3e60ffcf336b50f454b2a69085b
cecesc <celia.sanmartin.chapela@gmail.com>
2025-05-08 11:32:17 +0200

    Crear Actividad4.java

---
+++ src/ud7/apuntesjavafx/Actividad4.java
@@ -0,0 +1,36 @@
+package ud7.apuntesjavafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Actividad4 extends Application {
+    TextField txtColor = new TextField("Escribe un color (red, blue, #ff0000...)");
+    Button btnCambiar = new Button("Cambiar color");
+    VBox vbox = new VBox(txtColor, btnCambiar);
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public void cambiarColor() {
+        String color = txtColor.getText();
+        try {
+            vbox.setStyle("-fx-background-color: " + color + ";");
+        } catch (Exception ex) {
+            System.out.println("Color no válido");
+        }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        btnCambiar.setOnAction(e -> cambiarColor());
+        Scene scene = new Scene(vbox, 300, 150);
+        stage.setScene(scene);
+        stage.setTitle("Cambio de Color");
+        stage.show();
+    }
+}
