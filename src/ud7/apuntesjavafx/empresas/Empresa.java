229f18319d91e0ed6b0c6179358854d4718b2525
Celia Sanmartin <celia.sanmartin.chapela@gmail.com>
2025-05-14 00:20:48 +0200

    Arreglos en carpeta Empresa

--- src/ud7/apuntesjavafx/empresas/Empresa.java
+++ src/ud7/apuntesjavafx/empresas/Empresa.java
@@ -5,36 +5,29 @@ public class Empresa {
     private String nombre;
     private String web;
 
-
     public Empresa(int id, String nombre, String web) {
         this.id = id;
         this.nombre = nombre;
         this.web = web;
     }
 
-
     public Empresa(int id) {
         this.id = id;
     }
 
-
     @Override
     public String toString() {
-//        return id + " - " + nombre + " (" + web + ")";
+        // return id + " - " + nombre + " (" + web + ")";
         return id + " - " + nombre;
     }
-
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + id;
-        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-        result = prime * result + ((web == null) ? 0 : web.hashCode());
         return result;
     }
-
 
     @Override
     public boolean equals(Object obj) {
@@ -47,19 +40,31 @@ public class Empresa {
         Empresa other = (Empresa) obj;
         if (id != other.id)
             return false;
-        if (nombre == null) {
-            if (other.nombre != null)
-                return false;
-        } else if (!nombre.equals(other.nombre))
-            return false;
-        if (web == null) {
-            if (other.web != null)
-                return false;
-        } else if (!web.equals(other.web))
-            return false;
         return true;
     }
-    
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
 }
